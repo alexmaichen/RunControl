a05f62f27739b49e1f841a3ec7cecc70d0ea436c
alexmaichen <alexmaichen@gmail.com>
2026-03-23 20:31:12 +0100

    remove test file (ChaosTest.lua) from my hidden .gitignore

---
+++ RunControl/DemoRuns/ChaosTest.lua
@@ -0,0 +1,87 @@
+RunControl.Runs.ChaosTest = {
+    Metadata = {
+        Name = "ChaosTest",
+        Description = "test",
+        OriginalTime = "12:34.56",
+        OriginalHeat = "0",
+        CreatedFor = "1.2.1",
+    },
+    IndexedBy = { "chamberNum", "dataType", "rerollNum" },
+    List = {
+        [12] = {
+            roomFeatures = {
+                Data = {
+                    Flipped = false,
+                    ChaosGate = {
+                        Force = true,
+                    },
+                    ErebusGate = {
+                        Force = false,
+                    },
+                    Well = {
+                        Force = false,
+                    },
+                    SellWell = {
+                        Force = false,
+                    },
+                    Trove = {
+                        Force = false,
+                    },
+                    FishingPoint = {
+                        Force = false,
+                    },
+                },
+            },
+        },
+        [22] = {
+            roomFeatures = {
+                Data = {
+                    Flipped = false,
+                    ChaosGate = {
+                        Force = true,
+                    },
+                    ErebusGate = {
+                        Force = false,
+                    },
+                    Well = {
+                        Force = false,
+                    },
+                    SellWell = {
+                        Force = false,
+                    },
+                    Trove = {
+                        Force = false,
+                    },
+                    FishingPoint = {
+                        Force = false,
+                    },
+                },
+            },
+        },
+        [34] = {
+            roomFeatures = {
+                Data = {
+                    Flipped = false,
+                    ChaosGate = {
+                        Force = true,
+                    },
+                    ErebusGate = {
+                        Force = false,
+                    },
+                    Well = {
+                        Force = false,
+                    },
+                    SellWell = {
+                        Force = false,
+                    },
+                    Trove = {
+                        Force = false,
+                    },
+                    FishingPoint = {
+                        Force = false,
+                    },
+                },
+            },
+        },
+    },
+}
